b97a63265aec258b4edf1f6cd89ea4016c8c56a3
Adam Ayash <mobiragegames@mail.com>
2023-10-05 19:48:51 +0300

    Изтриване на ненужни файлове

---
+++ CourierManagementSystem/src/main/java/bg/tu_varma/sit/couriermanagementsystem/CourierManagementSystem.java
@@ -0,0 +1,15 @@
+package bg.tu_varma.sit.couriermanagementsystem;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CourierManagementSystem extends Application {
+    public static void main(String args[]){
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
