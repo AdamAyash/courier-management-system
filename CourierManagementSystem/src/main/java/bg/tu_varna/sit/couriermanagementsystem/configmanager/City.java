a1c31beeee70c9f7e3ceb43194a39919ab60c508
PavelSIT3A <pavel.boichev@abv.bg>
2023-10-22 16:39:11 +0300

    Добавяне на дискова структура на градове

---
+++ CourierManagementSystem/src/main/java/bg/tu_varna/sit/couriermanagementsystem/configmanager/City.java
@@ -0,0 +1,41 @@
+package bg.tu_varna.sit.couriermanagementsystem.configmanager;
+
+public class City {
+//-------------------------
+    //Constants:
+    //-------------------------
+
+    //-------------------------
+    //Members:
+    //-------------------------
+
+    private int _ID;
+
+    private String _name;
+
+    private String _state;
+
+    //-------------------------
+    //Properties:
+    //-------------------------
+
+    //-------------------------
+    //Constructor/Destructor:
+    //-------------------------
+    public City()
+    {
+
+    }
+    //-------------------------
+    //Methods:
+    //-------------------------
+
+    //-------------------------
+    //Overrides:
+    //-------------------------
+}
+
+
+
+
+
